7a503c4ce070dedf7f8a4596deff6f9d0af1e5b5
ngaro1830511 <ngaro1830511@etu.cegepjonquiere.ca>
2019-09-11 08:36:35 -0400

    Ajoutez des fichiers projet.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public class Monstre
+    {
+        string nom;
+        int pv;
+        string degat;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int Pv { get => pv; set => pv = value; }
+        public string Degat { get => degat; set => degat = value; }
+        public Monstre(string nom, int pv, string degat)
+        {
+            this.Nom = nom;
+            this.Pv = pv;
+            this.Degat = degat;
+        }
+        public override string ToString()
+        {
+            return "Le " + Nom + " qui a " + Pv + "pv et frappe de " + Degat + ".";
+        }
+        public partial class MainWindow : Window
+        {
+            public MainWindow()
+            {
+               // InitializeComponent();
+
+
+            }
+        }
+    }
+}
